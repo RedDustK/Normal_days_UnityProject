3145449c5523e93ffc29173415377e540a7f9b2f
Hong Jin Kim <emfoswu123@naver.com>
2023-09-13 00:47:11 +0900

    09.12 집

--- Assets/Scripts/ThirdPersonShootingController.cs
+++ Assets/Scripts/ThirdPersonShootingController.cs
@@ -3,25 +3,54 @@ using System.Collections.Generic;
 using UnityEngine;
 using Cinemachine;
 using StarterAssets;
+using UnityEngine.InputSystem;
+using UnityEditor.EventSystems;
 
 public class ThirdPersonShootingController : MonoBehaviour
 {
     [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
+    [SerializeField] private float normalSensitivity;
+    [SerializeField] private float aimSensitivty;
+    [SerializeField] private LayerMask aimColliderLayerMask = new LayerMask();
+    [SerializeField] private Transform debugTransform;
 
     private StarterAssetsInputs StarterAssetsInputs;
+    private ThirdPersonController thirdPersonController;
     private void Awake()
     {
         StarterAssetsInputs= GetComponent<StarterAssetsInputs>();
+        thirdPersonController= GetComponent<ThirdPersonController>();
     }
     private void Update()
     {
-        if(StarterAssetsInputs.aim) 
+        Vector3 mouseWorldPosition =Vector3.zero;
+        Vector2 screenCenterPoint = new Vector2 (Screen.width/2f, Screen.height/2f);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenterPoint);
+        if (Physics.Raycast(ray, out RaycastHit raycastHit, 999f, aimColliderLayerMask))
+        {
+            debugTransform.position = raycastHit.point;
+            mouseWorldPosition = raycastHit.point;
+        }
+
+
+        if (StarterAssetsInputs.aim) 
         {
             aimVirtualCamera.gameObject.SetActive(true);
+            thirdPersonController.SetSensitivitty(aimSensitivty);
+
+            Vector3 worldAimTarget = mouseWorldPosition;
+            worldAimTarget.y = transform.position.y;
+            Vector3 aimDirection = (worldAimTarget-transform.position).normalized;
         }
         else
         {
             aimVirtualCamera.gameObject.SetActive(false);
+            thirdPersonController.SetSensitivitty(normalSensitivity);
         }
+
+        
+        
+      
     }
+
 }
