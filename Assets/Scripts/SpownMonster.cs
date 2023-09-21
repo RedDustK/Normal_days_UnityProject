c11d3d9d252a406421a2bfed4cd9f1bee88e1144
Hong Jin Kim <emfoswu123@naver.com>
2023-09-22 07:21:54 +0900

    0922

--- Assets/Scripts/SpownMonster.cs
+++ Assets/Scripts/SpownMonster.cs
@@ -11,17 +11,6 @@ public class SpownMonster : MonoBehaviour
     bool SpawnStart;
     public GameObject SpawnPlace;
     IEnumerator enumerator;
-    // Start is called before the first frame update
-    void Start()
-    {
-       
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private IEnumerator SpawnMonster()
     {
