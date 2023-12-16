e4dd7b8e5c0e3d99627bf7795e4449b86e8a492e
RedDustK <emfoswu123@naver.com>
2023-12-16 15:36:01 +0900

    Fix : 안쓰는 코드 삭제

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -7,16 +7,6 @@ public class Unit : MonoBehaviour
     public int HP;
     public int MaxHP;
     public int AttackDamage;
-    // Start is called before the first frame update
-    void Start()
-    {
-        //HP = MaxHP;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
  
+  
 }
